3cca8bb896fafc264bd65816aa045a1a05cae796
0rayn <taha.eddafili@gmail.com>
2025-07-23 15:57:11 +0100

    okay

--- src/main.c
+++ src/main.c
@@ -1,12 +1,7 @@
 # include "drivers/led.h"
 
 __attribute__((noreturn)) void kmain(void) {
-    stat_led_init();
-
-    while (1) {
-        stat_led_on();
-        for (volatile int i = 0; i < 1000000; i++);
-        stat_led_off();
-        for (volatile int i = 0; i < 1000000; i++);
+    while (true){
+        
     }
 }
